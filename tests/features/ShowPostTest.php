b2647236db541c15e678d4bfd60bd95713ae7ccc
alejandro <aborrell@animalear.com>
2017-10-06 11:20:25 +0200

    Add redirection on old post urls

--- tests/features/ShowPostTest.php
+++ tests/features/ShowPostTest.php
@@ -7,7 +7,6 @@ class ShowPostTest extends FeatureTestCase
     function test_a_user_can_see_the_post_details()
     {
         // Having
-
         $user = $this->defaultUser([
             'name' => 'Alejandro Borrell'
         ]);
@@ -20,9 +19,30 @@ class ShowPostTest extends FeatureTestCase
         $user->posts()->save($post);
 
         // When
-        $this->visit(route('posts.show', $post))
+        $this->visit($post->url)
             ->seeInElement('h1', $post->title)
             ->see($post->body)
             ->see($user->name);
     }
+
+    function test_old_urls_are_redirected()
+    {
+        // Having
+        $user = $this->defaultUser();
+
+        $post = factory(Post::class)->make([
+            'title' => 'Old title'
+        ]);
+
+        $user->posts()->save($post);
+
+        $url = $post->url;
+
+        $post->update(['title' => 'New title']);
+
+        $this->visit($url)
+            ->seePageIs($post->url);
+
+    }
+
 }
